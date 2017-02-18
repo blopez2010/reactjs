b20482366cae4f2cbdbac06b77592fea99f1c444
Byron Lopez <merlin2010@gmail.com>
2017-02-18 13:35:17 -0600

    Working with child components

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component, PropTypes } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    };
+  }
+
+  update(e ){
+    this.setState({txt: e.target.value});
+  }
+
+  render() {
+    let txt = this.props.txt;
+    return <div>
+        <h1>{this.state.txt} - {this.state.cat}</h1>
+        <Widget update={this.update.bind(this)}></Widget>
+      </div>
+  }
+}
+
+const Widget = (props) => <input type="txt" onChange={props.update}/>
+
+App.propTypes = {
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "this is the default text"
+}
+
+export default App;
