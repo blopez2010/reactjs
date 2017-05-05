9ca889669eb09ae8c81931f15055f6f96d7a64de
Byron Lopez <merlin2010@gmail.com>
2017-05-05 13:06:46 -0600

    Adding reusable component.

--- src/Advance/Reuse.js
+++ src/Advance/Reuse.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
 class Reuse extends Component {
   constructor() {
     super();
     this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
+      red: 0
     }
 
     this.update = this.update.bind(this);
@@ -15,40 +13,62 @@ class Reuse extends Component {
 
   update(e) {
     this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
     });
   }
   render() {
     return (
       <div>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br/>
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br/>
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br/>
-      </div>
-    );
-  }
-}
-
-class Slider extends Component {
-  render() {
-    return (
-      <div>
-        <input type="range" ref="inp"
+        <NumInput
+          ref="red"
           min="0"
           max="255"
-          onChange={this.props.update} />
+          step={0.01}
+          val={+this.state.red}
+          label="Red"
+          update={this.update} />
       </div>
     );
   }
 }
 
+class NumInput extends Component {
+  render() {
+    let label = this.props.label !== '' ?
+      <label>{this.props.label} - {this.props.val}</label> : ''
+    return (
+      <div>
+        <input
+          ref="inp"
+          type={this.props.type}
+          min={this.props.min}
+          max={this.props.max}
+          step={this.props.step}
+          defaultValue={this.props.val}
+          onChange={this.props.update} />
+        {label}
+      </div>
+    );
+  }
+}
+
+NumInput.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+  step: PropTypes.number,
+  val: PropTypes.number,
+  label: PropTypes.string,
+  update: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(['number', 'range'])
+};
+
+NumInput.defaultProps = {
+  min: 0,
+  max: 0,
+  step: 0,
+  val: 0,
+  label: '',
+  type: 'range'
+};
 
 export default Reuse;
